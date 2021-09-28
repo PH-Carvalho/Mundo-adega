88fd5cbb495517885e8b7b9e66336608fb2fc9de
Pedro Henrique Carvalho <pedroflade2016@hotmail.com>
2021-09-27 22:43:30 -0300

    27/09/2021
    Aqui foi umas mudanças simples para acertar a responsividade do conteúdo, além de adicionar o alert básico de java Script dando alguns detalhes sobre o site

---
+++ index.js
@@ -0,0 +1,1 @@
+alert("Olá seja bem vindo!, esse conteúdo ainda está em produção portanto algumas página estão incompletas ou até mesmo não existe. O conteúdo está sujeito a mundaças. Lembre-se que esse projeto é totalmente feito para meios de estudos. ");
